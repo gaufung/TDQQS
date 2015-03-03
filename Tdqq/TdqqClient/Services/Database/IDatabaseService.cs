5b52ad603c86e2c01a67d966cd740e2aa35c69ac
gaufung <gaufung@foxmail.com>
2015-03-03 16:51:06 +0800

    first commit

---
+++ Tdqq/TdqqClient/Services/Database/IDatabaseService.cs
@@ -0,0 +1,30 @@
+﻿using System.Data;
+namespace TdqqClient.Services.Database
+{
+    /// <summary>
+    /// 定义数据库操作借口
+    /// </summary>
+    interface IDatabaseService
+    {
+        /// <summary>
+        /// 查询操作
+        /// </summary>
+        /// <param name="sqlString">SQL执行语句</param>
+        /// <returns>数据表</returns>
+        DataTable Query(string sqlString);
+
+        /// <summary>
+        /// 执行操作
+        /// </summary>
+        /// <param name="sqlString">SQL执行语句</param>
+        /// <returns>执行操作的数目</returns>
+        int Execute(string sqlString);
+
+        /// <summary>
+        /// 返回单个值的执行方法
+        /// </summary>
+        /// <param name="sqlString"></param>
+        /// <returns></returns>
+        object ExecuteScalar(string sqlString);
+    }
+}
---
+++ Tdqq/TdqqClient/Services/Database/MsAccessDatabase.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+
+namespace TdqqClient.Services.Database
+{
+    /// <summary>
+    /// Access数据库操作
+    /// </summary>
+    class MsAccessDatabase:IDatabaseService
+    {
+        private readonly  string _basicDatabase;
+        public MsAccessDatabase(string basicDatabase)
+        {
+            _basicDatabase = basicDatabase;
+        }
+        private OleDbConnection GetConnection()
+        {
+            try
+            {
+                string connnectString = "Provider=Microsoft.Jet.OLEDB.4.0;" + "data source=" + _basicDatabase;
+                var con = new OleDbConnection(connnectString);
+                return con;
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+        public System.Data.DataTable Query(string sqlString)
+        {
+            var con = GetConnection();
+            if (con == null) return null;
+            con.Open();
+            var dt = new DataTable();
+            if (con.State != ConnectionState.Open)return null;
+            try
+            {
+                var adapter = new OleDbDataAdapter(sqlString,con);              
+                adapter.Fill(dt);
+            }
+            catch (Exception)
+            {
+                dt = null;
+            }
+            finally
+            {
+                con.Close();               
+            }
+            return dt;
+        }
+        public int Execute(string sqlString)
+        {
+            const int errorState = -1;
+            var con = GetConnection();
+            if (con == null) return errorState;
+            con.Open();
+            if (con.State != ConnectionState.Open) return errorState;
+            int ret;
+            try
+            {
+                var cmd = new OleDbCommand(sqlString, con);
+                ret = cmd.ExecuteNonQuery();
+            }
+            catch (Exception)
+            {
+                ret = errorState;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return ret;
+        }
+        public object ExecuteScalar(string sqlString)
+        {
+            var con = GetConnection();
+            //数据库连接对象为空
+            if (con == null)return null;
+            con.Open();
+            if (con.State != ConnectionState.Open) return null;
+            var ret = new object();
+            try
+            {
+                var cmd = new OleDbCommand(sqlString, con);
+                ret = cmd.ExecuteScalar();
+            }
+            catch (Exception)
+            {
+                ret = null;
+            }
+            finally
+            {
+                con.Close();
+            }
+            return ret;
+        }
+    }
+}
