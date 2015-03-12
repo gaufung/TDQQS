1d0ad23a3c87b4912ffd630af7ccd08bf2b40698
gaufung <gaufung@foxmail.com>
2015-03-12 16:11:45 +0800

    fieldModel added

---
+++ Tdqq/TdqqClient/Models/FieldModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TdqqClient.Models
+{
+    
+
+    public  class FieldModel
+    {
+        public object ObjectId { get; set; }
+        public string Dkmc { get; set; }
+        public string Cbfmc { get; set; }
+        public string Dkbm { get; set; }
+        public double Scmj { get; set; }
+        public double Htmj { get; set; }
+        public string Dkdz { get; set; }
+        public string Dknz { get; set; }       
+        public string Dkxz { get; set; }
+        public string Dkbz { get; set; }
+
+        public double Yhtmj { get; set; }
+
+
+    }
+}
